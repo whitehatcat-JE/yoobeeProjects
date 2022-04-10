f321f2c8e86b3f698e3d5785f1dc4c7550f059b0
Jayden <everesja@gmail.com>
2022-04-10 22:55:56 +1200

    Added links in search results on homepage
    Also added the missing svgs for homepage.

--- bookDepositoryRedesign/javascript/booksDB.js
+++ bookDepositoryRedesign/javascript/booksDB.js
@@ -45,7 +45,7 @@ var books = {
 	},
 	2:{
 		"name":"Seven Husbands of Evelyn Hugo",
-		"author":"Taylor Jenkins Reid",
+		"author":"Taylor Reid",
 		"price":"23.63",
 		"stars":[590017, 280737, 78571, 15530, 5977],
 		"blurb":"From the author of Daisy Jones & The Six in which a legendary film actress reflects on her relentless rise to the top and the risks she took, the loves she lost, and the long-held secrets the public could never imagine. Aging and reclusive Hollywood movie icon Evelyn Hugo is finally ready to tell the truth about her glamorous and scandalous life. But when she chooses unknown magazine reporter Monique Grant for the job, no one is more astounded than Monique herself. Why her? Why now? Monique is not exactly on top of the world. Her husband has left her, and her professional life is going nowhere. Regardless of why Evelyn has selected her to write her biography, Monique is determined to use this opportunity to jumpstart her career. Summoned to Evelyn's luxurious apartment, Monique listens in fascination as the actress tells her story. From making her way to Los Angeles in the 1950s to her decision to leave show business in the '80s, and, of course, the seven husbands along the way, Evelyn unspools a tale of ruthless ambition, unexpected friendship, and a great forbidden love. Monique begins to feel a very real connection to the legendary star, but as Evelyn's story near its conclusion, it becomes clear that her life intersects with Monique's own in tragic and irreversible ways. The Seven Husbands of Evelyn Hugo is a mesmerizing journey through the splendour of old Hollywood into the harsh realities of the present day as two women struggle with what it means and what it costs to face the truth.",
